37ea0f1a932daa03381d880ade4babcd3173d24e
rausNT <ratunov@gmail.com>
2023-07-15 12:33:32 +0300

    cleen

--- update_base/AppDelegate.swift
+++ update_base/AppDelegate.swift
@@ -51,27 +51,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 
-//import Cocoa
-//
-//@main
-//class AppDelegate: NSObject, NSApplicationDelegate {
-//
-//
-//
-//
-//    func applicationDidFinishLaunching(_ aNotification: Notification) {
-//        // Insert code here to initialize your application
-//
-//    }
-//
-//    func applicationWillTerminate(_ aNotification: Notification) {
-//        // Insert code here to tear down your application
-//    }
-//
-//    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-//        return true
-//    }
-//
-//
-//}
+
 
